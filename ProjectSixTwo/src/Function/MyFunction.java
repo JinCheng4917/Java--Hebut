2035631eee7358e18deaddc003a1ba44f5ac8da8
JinCheng <jincheng@JinChengdeMacBook-Pro.local>
2020-10-20 15:02:27 +0800

    完善实验6-2

---
+++ ProjectSixTwo/src/Function/MyFunction.java
@@ -0,0 +1,66 @@
+package Function;
+
+import java.io.*;
+
+public class MyFunction {
+
+    /**
+     * 程序开始前清空文档数据
+     *
+     * @param file 要判断的文件
+     * @throws IOException
+     */
+    public void flushFile(File file) {
+        try {
+            FileReader fileReader = new FileReader(file);
+            if (file.length() > 0) {
+                System.out.println("文件" + file.getName() + "内容不为空，正在清空");
+                FileWriter fileWriter = new FileWriter(file);
+                char[] a = new char[0];
+                fileWriter.write(a);
+                fileWriter.close();
+                if (file.length() == 0) {
+                    System.out.println(file.getName() + "成功清空");
+                }
+            }
+            fileReader.close();
+        } catch (IOException e) {
+            System.out.println("程序运行失败，失败原因：" + e);
+        }
+
+    }
+
+    public void copyFile(File sourceFile, File targetFile) {
+        try {
+            FileReader fileReader = new FileReader(sourceFile);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            FileWriter fileWriter = new FileWriter(targetFile);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null) {
+                bufferedWriter.write(line);
+                bufferedWriter.newLine(); //因为每次是读取一行内容，所以显示一行后就得换行
+                bufferedWriter.flush(); //刷新内容
+            }
+            fileWriter.close();
+            fileReader.close();
+        } catch (IOException e) {
+            System.out.println("文件复制过程发生错误");
+        }
+
+    }
+
+    public void readFile(File file) {
+        try {
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line = null;
+            System.out.println(file.getName());
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("文件读取失败，失败原因： " + e);
+        }
+    }
+}
--- ProjectSixTwo/src/Main/ProjectSixTwo.java
+++ ProjectSixTwo/src/Main/ProjectSixTwo.java
@@ -1,20 +1,21 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+package Main;
+
+import Function.MyFunction;
+
+import java.io.*;
 import java.nio.file.Files;
 import java.util.Scanner;
 
 public class ProjectSixTwo {
     public static void main(String[] args) throws IOException {
+        System.out.println("程序开始运行");
+        MyFunction myFunction = new MyFunction();
+
         File myFile1 = new File("myFile1.txt");
         File myFile2 = new File("myFile2.txt");
-        //如果文件存在,则删除
-        if (myFile1.exists()) {
-            myFile1.delete();
-        }
-        if (myFile2.exists()) {
-            myFile2.delete();
-        }
+
+        myFunction.flushFile(myFile1);
+        myFunction.flushFile(myFile2);
         try {
             FileWriter fileWriter1 = new FileWriter(myFile1);
             fileWriter1.write("姓名    专业   班级    家庭住址     学号    \n");
@@ -30,17 +31,23 @@ public class ProjectSixTwo {
                 //写入文件只能以字符形式写入
                 fileWriter1.write(String.valueOf(scanner.nextInt()));
                 fileWriter1.write("\n");
-                System.out.println("请确定是否结束：bye-结束  next-继续");
-                Scanner selectScanner = new Scanner(System.in);
-                String mySelect = selectScanner.next();
-                if (mySelect.equals("bye")) {
+                System.out.println("请确定是否结束：bye-结束  回车-继续");
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+                String selectScanner  = bufferedReader.readLine();
+                if (selectScanner.equals("bye")) {
                     break;
-                } else if (mySelect.equals("next")) {
+                } else if (selectScanner.length() == 0) {
                     continue;
                 }
             }
             fileWriter1.close();
-            Files.copy(myFile1.toPath(), myFile2.toPath());
+            myFunction.copyFile(myFile1, myFile2);
+            System.out.println("文件复制成功，文件内容为：");
+            System.out.println("");
+            myFunction.readFile(myFile1);
+            System.out.println("");
+            System.out.println("");
+            myFunction.readFile(myFile2);
         } catch (IOException e) {
             System.out.println(e);
         }
