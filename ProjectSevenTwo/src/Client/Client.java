685b2e6004bdd64cf9c330bae118e58e6242c306
zhangwenda4917 <3300491785@qq.com>
2020-11-05 16:38:35 +0800

    完成实验7-2

--- ProjectSevenTwo/src/Client/Client.java
+++ ProjectSevenTwo/src/Client/Client.java
@@ -1,36 +1,97 @@
 package Client;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.Socket;
-import java.util.Scanner;
+import java.util.Random;
 
-public class Client {
-    private static int port = 4500;
-    public static void main(String[] args) {
+import javax.swing.*;
+
+
+public class Client extends JFrame implements ActionListener, Runnable {
+    private static int PORT = 4500;
+    private String name;
+    private JTextArea textArea;
+    private JTextField textField;
+    private PrintWriter printWriter;
+    private JButton[] buttons;
+    private Socket socket;
+
+    public Client(String name, Socket socket) throws IOException {
+        super("通讯窗口 " + name + InetAddress.getLocalHost() + ":" + socket.getLocalPort());
+        this.setBounds(320, 240, 400, 240);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.textArea = new JTextArea();
+        this.textArea.setEditable(false);
+        this.getContentPane().add(new JScrollPane(this.textArea));
+
+        JToolBar toolBar = new JToolBar();
+        this.getContentPane().add(toolBar, "South");
+        toolBar.add(this.textField = new JTextField(30));
+        String[] buttonStr = {"发送", "离线"};
+        buttons = new JButton[buttonStr.length];
+        for (int i = 0; i < buttonStr.length; i++) {
+            toolBar.add(buttons[i] = new JButton(buttonStr[i]));
+            buttons[i].addActionListener(this);
+        }
+        this.setVisible(true);
+        this.name = name;
+        this.socket = socket;
+        new Thread(this).start();
+    }
+
+    @Override
+    public void run() {
         try {
-            Socket socket = new Socket("127.0.0.1", port);
+            this.printWriter = new PrintWriter(socket.getOutputStream(), true);
+            this.printWriter.println(name);
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
-            printWriter.println("客户端连接成功");
-            String clientInput;
-            Scanner scanner = new Scanner(System.in);
-            do {
-                clientInput = bufferedReader.readLine();
-                System.out.println("接收到服务端消息");
-                System.out.println(clientInput);
-                System.out.println("向服务端发送消息");
-                printWriter.println(scanner.nextLine());
-            }while (!clientInput.equals("send"));
+            String line = "连接" + bufferedReader.readLine() + "成功";
+            while (line != null && !line.endsWith("Bye")) {
+                textArea.append(line + "\r\n");
+                line = bufferedReader.readLine();
+            }
             bufferedReader.close();
-            printWriter.close();
+            this.printWriter.close();
             socket.close();
-
-        }catch (IOException e) {
+            buttons[0].setEnabled(false);
+            buttons[1].setEnabled(false);
+        } catch (IOException e) {
 
         }
     }
 
+    public Client(String name, String host, int port) throws IOException {
+        this(name, new Socket(host, port));
+    }
+
+    public void actionPerformed(ActionEvent actionEvent) {
+        if (actionEvent.getActionCommand().equals("发送")) ;
+        {
+            this.printWriter.println(name + "说： " + textField.getText());
+            this.textArea.append("向服务端发送：" + textField.getText() + "\n");
+            textField.setText("");
+        }
+        if (actionEvent.getActionCommand().equals("离线")) {
+            this.textArea.append("本机离线\n");
+            this.printWriter.println(name + "离线\n" + "Bye\n");
+            buttons[0].setEnabled(false);
+            buttons[1].setEnabled(false);
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            new Client("客户端" + new Random().nextInt(), "127.0.0.1", PORT);
+            new Client("客户端" + new Random().nextInt(), "127.0.0.1", PORT);
+            new Client("客户端" + new Random().nextInt(), "127.0.0.1", PORT);
+        } catch (IOException e) {
+
+        }
+    }
 }
--- ProjectSevenTwo/src/Server/Server.java
+++ ProjectSevenTwo/src/Server/Server.java
@@ -1,41 +1,29 @@
 package Server;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
+
+import Client.Client;
 
 public class Server {
-    private static int port = 4500;
-    public static void main(String[] args) {
-        try {
-            ServerSocket serverSocket = new ServerSocket( port);
-            System.out.println("等待客户端连接");
-            Socket socket = serverSocket.accept();
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
-            printWriter.println("服务端连接成功");
-            Scanner scanner = new Scanner(System.in);
-            String serverInput;
-            do {
-                serverInput = bufferedReader.readLine();
-                System.out.println("接收到客户端消息");
-                if (!serverInput.equals("客户端连接成功")) {
-                    System.out.println(serverInput);
-                    System.out.println("向客户端发送消息");
-                    printWriter.println(scanner.nextLine());
-                }
-            }while (!serverInput.equals("send"));
-            bufferedReader.close();
-            printWriter.close();
-            socket.close();
+    private static int PORT = 4500;
 
-        }catch (IOException e) {
+    public Server(int port, String name) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            System.out.println("等待客户端连接");
+            while (true) {
+                Socket socket = serverSocket.accept();
+                new Client(name, socket);
+            }
+        } catch (IOException e) {
 
         }
+    }
+
+    public static void main(String[] args) {
+        new Server(PORT, "服务端");
     }
 
 }
